fb67d801ff05c79a51eada5c263870939d935148
Tim Porbadnigk <134412834+Acoiny@users.noreply.github.com>
2025-11-18 15:28:23 +0100

    added mensatime and settings

---
+++ src/components/MensaTime.vue
@@ -0,0 +1,116 @@
+<script setup lang="ts">
+import { LoadSettings, SaveSettings, AutoRedirect } from '../settings.ts';
+
+if(!AutoRedirect()) {
+  const setts = LoadSettings();
+  setts.playingMensatime = true
+  SaveSettings(setts);
+}
+
+const audio = new Audio('/audio/mensatime.mp3');
+audio.play().finally(() => {
+  const setts = LoadSettings();
+  setts.playingMensatime = false;
+  SaveSettings(setts);
+});
+
+</script>
+
+<template>
+  <div class="container">
+    <h1>It's Mensatime!</h1>
+    <iframe src="https://stwno.de/infomax/daten-extern/html/speiseplaene.php?einrichtung=HS-R-tag" title="Mensaplan"/>
+    <br>
+    <br>
+    <h2>Lyrics</h2>
+    <pre>
+Money Boy Swag, das ist Money Boy Rap
+Es ist Mensatime und es wird ein Money Boy Fest
+Und ich cruise durch die Town in meinem Maybach denn
+Es dauert nicht mehr lang und schon ist es Mensatime
+Yeah, triff mich in der Stadt, Homie, ja ich gebe Geld aus
+Guck mal wie ich meiner Lady einen schönen Held kauf'
+Rotfuchs, Silberfuchs, kuschelweiches Fell
+Und danach fick ich kurz mit einer Nutte im Hotel
+Yeah, PRPS, Bitches finden mеine Rosen fresh
+Ich hab dеn Mensa Swag, grüner Schal, rote Cap
+Es ist Mensatime, Party jede Nacht, Slime
+Und zwar die ganze Nacht, wir gehen erstmal um acht Heim
+Happy Holidays, ich flieg nach Hawaii, man
+Zu Silvester geht es weiter, dann nach Thailand
+Yeah, ich feier Mensatime am Strand
+Ich kann's kaum erwarten, ich bin schon auf Mensatime gespannt
+
+Es ist Mensatime, es ist Mensatime
+Es ist Mensatime, ich lad ein paar Bitches ein
+Ich lad ein paar Bitches ein, fick sie unter Mistenzweig
+Es ist Mensa, es ist Mensa, Es ist Mensatime
+Es ist Mensatime, es ist Mensatime
+Es ist Mensatime, ich lad ein paar Bitches ein
+Ich lad ein paar Bitches ein, fick sie unter Mistenzweig
+Es ist Mensa, es ist Mensa, Es ist Mensatime
+
+Es ist Mensatime, Mensazeit, frohes Fest
+Ich trage eine grüne Weste und ein rotes Cap
+Es ist Mensazeit, überall weißer Schnee
+Und ich ziehe eine Line, homie, live on stage
+Es ist Heiligabend, ich trage Designermarken
+Du willst auch gerne so 'ne coole Kleidung haben
+Das ist auch okay, schreib doch eine Wunschliste
+Ich hab Swag Homie, triff mich vor der Punschhütte
+Mr. Moneyboy, Merry Merry Mensa
+Und am Mensabaum, very sexy Bitches
+Schöne Ladies in heißem Dessous
+Mr. Moneyboy, ja ich finde Mensatime cool
+Und ich spritz euch an, an mit meinem Mensa-Swag
+Bitches stehen auf Stöckelschuhe und Designerbags
+Darum kauf ich ihnen Jimmy Choo's, Loui' Stuff
+Und zum drüberstreuen, eine nice Gucci Clutch
+
+Es ist Mensatime, es ist Mensatime
+Es ist Mensatime, ich lad ein paar Bitches ein
+Ich lad ein paar Bitches ein, fick sie unter Mistenzweig
+Es ist Mensa, es ist Mensa, Es ist Mensatime
+Es ist Mensatime, es ist Mensatime
+Es ist Mensatime, ich lad ein paar Bitches ein
+Ich lad ein paar Bitches ein, fick sie unter Mistenzweig
+Es ist Mensa, es ist Mensa, Es ist Mensatime
+      </pre>
+  </div>
+</template>
+
+<style scoped>
+
+.container {
+  margin-left: 30%;
+  margin-right: 30%;
+  padding: 0 20px;
+}
+
+iframe {
+  height: 50vh;
+  width: 50vw;
+  display: block;
+  border-style: none;
+}
+
+@media(max-width: 768px) {
+  .container {
+    margin-left: 5%;
+    margin-right: 5%;
+    padding: 0 15px;
+  }
+
+  iframe {
+    width: 80vw;
+  }
+
+  h1 {
+    font-size: 2.5em;
+  }
+
+  h2 {
+    font-size: 1.8em;
+  }
+}
+</style>
---
+++ src/settings.ts
@@ -0,0 +1,56 @@
+import { router } from './router.ts';
+
+const SETTINGS_KEY = "settings";
+
+// settings for the whole website
+// if any new persistent settings are needed,
+// they are to be added to this class
+type Settings = {
+  playingMensatime: boolean
+};
+
+const defaultSettings: Settings = {
+  playingMensatime: false,
+};
+
+// the current settings
+let settings: Settings
+{
+  const res = localStorage.getItem(SETTINGS_KEY);
+  if (!res) {
+    settings = defaultSettings;
+  } else {
+    try {
+      settings = JSON.parse(res);
+    } catch (e) {
+      settings = defaultSettings;
+    }
+  }
+}
+
+export function LoadSettings(): Settings {
+  return settings;
+}
+
+export function SaveSettings(sett: Settings): void {
+  localStorage.setItem(SETTINGS_KEY, JSON.stringify(sett));
+  settings = sett;
+}
+
+/**
+ * Automatically redirects to the currently relevant page, if
+ * that should be necessary
+ *
+ * @returns true, if a redirect has happened
+*/
+export function AutoRedirect(): boolean {
+  let redirected = false;
+
+  // redirect to mensatime
+  if (settings.playingMensatime) {
+    router.replace('/mensatime');
+    redirected = true;
+  }
+
+  return redirected;
+}
